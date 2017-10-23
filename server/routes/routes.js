af0c482c16074dd929aac9804307fd40eac1badd
Arthur B. de Oliveira <arthurboliveira93@gmail.com>
2017-10-23 18:23:30 -0200

    New features

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 let {Post} = require('./../models/post');
 
 module.exports = (app, passport) => {
@@ -8,4 +10,17 @@ module.exports = (app, passport) => {
             res.status(400).send(e);
         });        
     });
+
+    //#region Posts
+    app.post('/post', (req, res) => {
+        let body = _.pick(req.body, ['title', 'text', 'img']);
+        let post = new Post(body);
+
+        post.save().then((post) => {
+            res.send(post);
+        }).catch((e) => {
+            res.status(400).send(e);
+        });
+    });
+    //#endregion
 };
